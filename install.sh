ae13fb37fff0d2acc25839635aa08034daffbe77
5trange <adithmurali@protonmail.com>
2022-02-05 18:23:49 +0530

    add pkglist.txt + update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install_base() {
 	echo 'Remember to run this script as superuser!'
 	refresh_repos
     	echo 'Installing packages...'
-	pacman -S bpswm sxhkd rofi papiprus-icon-theme arc-gtk-theme dunst alacritty pcmanfm ttf-font-awesome ttf-jetbrains-mono ttf-liberation nitrogen viewnior polkit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+	pacman -S --needed --noconfirm - < pkglist.txt
 }
 
 refresh_repos() {
@@ -12,8 +12,5 @@ refresh_repos() {
 	pacman -Sy
 }
 
-setup_lightdm() {
-	systemctl enable lighdm.service
-}
-
+refresh_repos
 install_base
