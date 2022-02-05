a928b8c3bdd64f7a03186a0f51fe52dedc2798ce
5trange <adithmurali@protonmail.com>
2022-02-05 19:06:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-install_base() {
-	echo 'Remember to run this script as superuser!'
-	refresh_repos
-    	echo 'Installing packages...'
-	pacman -S --needed --noconfirm - < pkglist.txt
-}
-
-refresh_repos() {
-	echo 'Refreshing repositories...'
-	pacman -Sy
-}
-
 setup_yay() {
+	sudo pacman -S --needed --noconfirm base-devel
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
-	makepkg -si
+	makepkg -si --noconfirm
 	cd ..
 	rm -rf yay
 }
 
+refresh_repos() {
+	echo '*** Refreshing repositories... ***'
+	sudo pacman -Sy
+}
+
+install_base_pkgs() {
+    echo '*** Installing packages... ***'
+	sudo pacman -S --needed --noconfirm - < pkglist.txt
+}
+
+install_aur_pkgs() {
+	echo '*** Installing AUR packages... ***'
+	yay -S --noconfirm polybar ly
+	echo '*** Cleaning up... ***'
+	yay -Yc --noconfirm
+}
+
+setup_ly_service() {
+	echo '*** Enabling ly service... ***'
+	sudo systemctl enable ly.service
+}
+
+install_config() {
+	echo '*** Installing config... ***'
+	cp -r .config/ ~
+	cp -r .local/ ~
+}
+
 setup_yay
 refresh_repos
-install_base
+install_base_pkgs
+install_aur_pkgs
+setup_ly_service
+install_config
+echo '*** Install complete! ***'
+echo '*** You might want to install display drivers though. ***'
